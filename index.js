de372561e34a2549a429c1d97258799087d14b13
foysal41 <mdfoysal.mf33@example.com>
2026-05-12 21:29:40 +0600

    comment client db

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
   
     const db = client.db("wanderlust")
     const destinationCollection = db.collection('destination')
@@ -83,7 +83,7 @@ async function run() {
 
 
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
     console.log(err);
